a85acab39998c50073056becbd130da8c3b719da
cmcool <75821231+cmcool@users.noreply.github.com>
2021-09-16 10:51:30 -0400

    Update main.py

--- main.py
+++ main.py
@@ -2,11 +2,13 @@ import pygame
 from pygame import *
 import sys
 import random
+import time
 
 # window setup
 win = pygame.Surface
 WIDTH = 700
 HEIGHT = 700
+gameDisplay = pygame.display.set_mode((WIDTH,HEIGHT))
 
 # song setup
 pygame.mixer.pre_init(44100, -16, 2, 2048)
@@ -22,7 +24,6 @@ BLACK = (0, 0, 0)
 RED = (255, 0, 0)
 GREEN = (0, 255, 0)
 BLUE = (66, 135, 245)
-
 
 class Player(pygame.sprite.Sprite): # player sprite
     def __init__(self):
@@ -50,6 +51,22 @@ class Mob(pygame.sprite.Sprite): # enemy sprite
             self.rect.y = random.randrange(-100, -40)
             self.speedy = random.randrange(1, 2)
 
+def text_objects(text, font):
+        textSurface = font.render(text, True, WHITE)
+        return textSurface, textSurface.get_rect()
+
+def message_display(text):# allows you to show text to user using function
+    largeText = pygame.font.Font('ROGFontsv1.6-Regular.ttf',115)
+    TextSurf, TextRect = text_objects(text, largeText)
+    TextRect.center = ((WIDTH/2),(HEIGHT/2))
+    gameDisplay.blit(TextSurf, TextRect)
+
+    pygame.display.update()
+
+def crash(): # game over function
+    running = False
+    message_display('Game Over')
+
 # initialize pygame and create window
 pygame.init()
 screen = pygame.display.set_mode((WIDTH, HEIGHT))
@@ -70,25 +87,26 @@ for i in range(5):
 # Game loop
 running = True
 while running:
+    mixer.music.set_volume(0.3)
     # Process input
     for event in pygame.event.get():
         # check for closing window
         if event.type == pygame.QUIT:
             pygame.quit()
             sys.exit()
-        # sprite controls
 
+        # sprite controls
         elif event.type == pygame.KEYDOWN:
             if event.key == pygame.K_UP or event.key == K_w:
                 player.rect.centery -= 2
+            if event.key == pygame.K_SPACE:
+                running = True
             elif event.key == pygame.K_DOWN or event.key == K_s:
                 player.rect.centery += 2
             elif event.key == pygame.K_LEFT or event.key == K_a:
                 player.rect.left -= 2
             elif event.key == pygame.K_RIGHT or event.key == K_d:
-                # basically, if (sprite.x + sprite.width < screen.width): sprite.x += 5
                 if player.rect.x + player.image.get_width() < WIDTH:
-                    # not sure if methods are correct
                     player.rect.right += 2
 
     # Update
@@ -96,8 +114,12 @@ while running:
 
     # hitboxes
     hits = pygame.sprite.spritecollide(player, mobs, False)
-    if hits:
-        running = False
+
+    if hits: # game over sequence
+        crash()
+        mixer.music.set_volume(0.1)
+        time.sleep(1)
+
 
     # Draw / render
     screen.fill((18,18,18))
